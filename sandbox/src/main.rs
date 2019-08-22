564991dfed2879b81c42b7abe0d5dc0f9c01bd65
Mathias Magnusson <mathias@beastpop.localdomain>
2019-08-22 23:22:57 +0200

    Testing some of the new stuff

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,26 +1,46 @@
-use wrath::Engine;
+#![feature(box_syntax)]
+
 use wrath::CallbackHandler;
+use wrath::Engine;
+use wrath::Layer;
+use wrath::LayerHandle;
 
 struct Application {
-
+	ex_layer: LayerHandle,
 }
 
 impl Application {
 	fn new() -> Self {
-		Application {}
+		Application {
+			ex_layer: LayerHandle::none(),
+		}
 	}
 }
 
 impl CallbackHandler for Application {
 	fn on_create(&mut self, engine: &mut Engine) {
-		println!("Hello");
 		engine.create_window(wrath::WindowProps {
 			title: "Wrath Engine Sandbox".into(),
-			size: (1080, 720)
+			size: (1080, 720),
 		});
+		self.ex_layer = engine.layer_stack().push_front(box ExampleLayer::new());
 	}
-	fn on_exit(&mut self, _engine: &mut Engine) {
-		println!("Bye");
+	fn on_exit(&mut self, engine: &mut Engine) {
+		engine.layer_stack().remove_layer(self.ex_layer);
+	}
+}
+
+struct ExampleLayer { }
+
+impl ExampleLayer {
+	pub fn new() -> Self {
+		Self { }
+	}
+}
+
+impl Layer for ExampleLayer {
+	fn on_window_resize(&mut self, size: (u32, u32)) {
+		println!("Window resized: ({}, {})", size.0, size.1);
 	}
 }
 
